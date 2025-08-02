9993225039d9e97b0537376a75aa32429b890235
honeybody <honeybody@protonmail.com>
2025-08-02 11:44:26 -0500

    working js

---
+++ _site/bundle.js
@@ -0,0 +1,134 @@
+const DOMZineOuter = document.querySelector("[data-template='zine-pages']");
+
+const nextButtonWide = document.querySelector("[data-action='next-wide']");
+const prevButtonWide = document.querySelector("[data-action='prev-wide']");
+
+const nextButtonNarrow = document.querySelector("[data-action='next-narrow']");
+const prevButtonNarrow = document.querySelector("[data-action='prev-narrow']");
+
+let currentSpread = 0;
+let currentPage = 0;
+const pages = document.querySelectorAll(".zine-page");
+const totalSpreads = pages.length / 2;
+const totalPages = pages.length;
+const DOMZine = document.querySelector(".zine-inner");
+const DOMPages = document.querySelectorAll(".zine-page");
+
+function changeSpread(direction) {
+  // change the currentSpread number
+  currentSpread = currentSpread + 1 * direction;
+
+  // show the current pages
+  showSpread(currentSpread, direction);
+}
+
+function showSpread(spread, direction) {
+  const leftPage = DOMPages[spread * 2 - 1];
+  const rightPage = DOMPages[spread * 2];
+
+  // set data attribute for direction
+  if (direction === -1) {
+    DOMZine.dataset.direction = "decrement";
+  } else {
+    DOMZine.dataset.direction = "increment";
+  }
+
+  // hide previous pages
+  DOMPages.forEach((page) => {
+    page.classList.remove("animatingOut", "animatingIn");
+    if (page.classList.contains("visible")) {
+      page.classList.remove("visible");
+      page.classList.add("animatingOut");
+    }
+  });
+
+  // show right page
+  if (rightPage) {
+    rightPage.classList.add("visible");
+    rightPage.classList.add("animatingIn");
+  }
+
+  // show left page
+  if (leftPage) {
+    leftPage.classList.add("visible");
+    leftPage.classList.add("animatingIn");
+  }
+
+  // center align (or not) the shown pages
+  if (currentSpread === 0) {
+    DOMZine.classList.add("front-cover");
+  } else if (currentSpread === totalSpreads) {
+    DOMZine.classList.add("back-cover");
+  } else {
+    DOMZine.classList.remove("front-cover", "back-cover");
+  }
+}
+
+function changePage(direction) {
+  // increment the current page by 1
+  currentPage = currentPage + 1 * direction;
+  console.log(currentPage);
+  // when the second page in a spread is focused and incremented again, increase the spread.
+  if (direction === 1 && (currentPage % 2 === 1 || currentPage === 0)) {
+    currentSpread = currentSpread + 1;
+    showSpread(currentSpread, direction);
+    //   or, when the first page in a spread is focused and decremented again, decrease the spread
+  } else if (direction === -1 && currentPage % 2 === 0) {
+    currentSpread = currentSpread - 1;
+    showSpread(currentSpread, direction);
+  }
+
+  showPages(currentPage);
+}
+
+function showPages(page) {
+  if (page % 2 === 0) {
+    DOMZine.classList.add("right");
+  } else {
+    DOMZine.classList.remove("right");
+  }
+}
+
+//   wide screens navigation events
+nextButtonWide.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentSpread < totalSpreads) {
+    changeSpread(1);
+  }
+});
+
+prevButtonWide.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentSpread > 0) {
+    changeSpread(-1);
+  }
+});
+
+//   narrow screens navigation events
+nextButtonNarrow.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentSpread < totalSpreads) {
+    changePage(1);
+  }
+});
+
+prevButtonNarrow.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentSpread > 0) {
+    changePage(-1);
+  }
+});
+
+showSpread(0);
+
+const mq = window.matchMedia("(prefers-reduced-motion: no-preference)");
+
+if (mq.matches) {
+  document.addEventListener("mousemove", function (event) {
+    const aimX = (event.pageX - window.innerWidth / 2) / 100;
+    const aimY = (event.pageY - window.innerHeight / 2) / -100;
+    DOMZineOuter.style.setProperty("--aimX", `${aimX}`);
+    DOMZineOuter.style.setProperty("--aimY", `${aimY}`);
+  });
+}
+
