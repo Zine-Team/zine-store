72b6e99878e67246fc21c9238c9b37ac081ad225
honeybody <honeybody@protonmail.com>
2025-08-03 14:17:46 -0500

    acceptable for launch

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,10 @@ export default async function (eleventyConfig) {
   eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
   eleventyConfig.addLayoutAlias("zine", "layouts/zine.njk");
   eleventyConfig.addPassthroughCopy("images");
+
+  eleventyConfig.addFilter("slice", (array, number) => {
+    return array.slice(0, number);
+  });
 }
 
 export const config = {
